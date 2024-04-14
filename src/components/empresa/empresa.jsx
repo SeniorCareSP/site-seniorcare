abe5e5d5a12ad040a4deb1c98b5460bfe0417ea9
rafa18xx <rafaela.oliveira@sptech.school>
2024-04-14 01:21:55 -0300

    css superficial dos motivos

--- src/components/empresa/empresa.jsx
+++ src/components/empresa/empresa.jsx
@@ -1,13 +1,15 @@
+import Style from "./empresa.module.css"
+import React from "react";
 function Empresa() {
     return (
-        <div className="empresa">
+        <div className={Style["empresa"]}>
             <h2>Nós da Senior Care somos uma</h2>
             <div>
-                <span className="linha"></span>
+                <span className={Style["linha"]}></span>
                 <h1>Empresa</h1>
-                <span className="linha"></span>
+                <span className={Style["linha"]}></span>
             </div>
-            <div className="descricao">
+            <div className={Style["descricao"]}>
                 <p>
                     Comprometida em atender às necessidades crescentes de uma população que envelhece rapidamente. Em um mundo onde encontrar cuidadores confiáveis para os nossos entes queridos se tornou uma necessidade crucial, assumimos a responsabilidade de oferecer não apenas assistência, mas também conforto e confiança. A Senior Care não é apenas uma empresa; é uma promessa de qualidade, segurança e tranquilidade para sua família. Estamos aqui para garantir que seus entes queridos recebam o cuidado atencioso e dedicado que merecem.
                 </p>
--- src/components/servico/servico.jsx
+++ src/components/servico/servico.jsx
@@ -1,15 +1,19 @@
+import React from "react";
+import Style from "./servico.module.css"
 function Servico() {
     return (
-        <div className="servico">
-            <span className="obj1"></span>
+        <frameElement>
+        <div className={Style["servico"]}>
+            <span className={Style["obj1"]}></span>
             <h1>Serviços</h1>
             <p>
                 Não importa se você precisa de um cuidador em tempo integral para auxiliar em casa ou ocasionalmente durante a semana - o Senior Care ajuda você a se conectar e conversar com cuidadores qualificados perto de você.
             </p>
             <button>Comece agora</button>
-            <span className="obj2"></span>
-            <span className="obj3"></span>
+            <span className={Style["obj2"]}></span>
+            <span className={Style["obj3"]}></span>
         </div>
+        </frameElement>
     );
 }
 
