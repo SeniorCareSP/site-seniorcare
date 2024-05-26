73766c8511765c17dbcdef48d2e706ace78c50f6
Gustavo-s-machado <gustavo.smachado@sptech.school>
2024-05-26 20:43:56 -0300

    atualização das rotas

---
+++ src/components/charts/Dashboard.jsx
@@ -0,0 +1,47 @@
+import { Stack } from "@mui/material";
+import ChartGenero from "./ChartGenero";
+import ChartAvaliacao from "./ChartAvaliacao";
+import SidebarDash from "../sidebar/SidebarDash";
+
+function EleDashbord() {
+    return (
+        <>
+            <div>
+                <SidebarDash />
+                <Stack>
+                    <div>
+
+                    </div>
+                    <div>
+                        <ChartAvaliacao />
+                    </div>
+                </Stack>
+                <Stack>
+                    <Stack>
+                        <div>
+                            <h1>
+
+                            </h1>
+                            <h4>
+
+                            </h4>
+                        </div>
+                        <div>
+                            <h1>
+
+                            </h1>
+                            <h4>
+
+                            </h4>
+                        </div>
+                    </Stack>
+                    <div>
+
+                    </div>
+                </Stack>
+            </div>
+        </>
+    );
+}
+
+export default EleDashbord;
--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from "./pages/home/Home";
 import Login from './pages/login/Login';
-import Cadastro from "./pages/cadastro/Cadastro"
+import Cadastro from "./pages/cadastro/Cadastro";
 import Cadastro2 from './pages/cadastro/Cadastro2';
 import Cadastro3 from './pages/cadastro/Cadastro3';
 import Cuidador1 from './pages/cadastro/Cuidador1';
@@ -17,37 +17,36 @@ import Favorito from "./pages/cuidador/favorito";
 import AtualizarPerfil from './pages/confUser/AtualizarPerfil';
 import AnaliseDocs from './pages/admin/AnaliseDocs';
 import Erro from './pages/404/erro';
-import Perfil from './pages/Perfil/VisualizaoInterna'
- 
+import Perfil from './pages/Perfil/VisualizaoInterna';
+import Dashboard from './pages/admin/Dashboard';
 
 function Router() {
   return (
     <BrowserRouter>                                        
       <Routes>       
-				<Route path={"/"} element={<Home />} />             
-				<Route path="/login" element={<Login />} />    
+				<Route path="/" element={<Home/>}/>             
 				<Route path="*" element={<Erro/>}/>    
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path='/cadastro2' element={<Cadastro2 />} />
-        <Route path='/cadastro3' element={<Cadastro3 />} />
-        <Route path='/cadastro/cuidador' element={<Cuidador1 />} />
-        <Route path='/cadastro/cuidador2' element={<Cuidador2 />} />
-        <Route path='/cadastro/cuidador3' element={<Cuidador3 />} />
-        <Route path='/procurar' element={<Procurar />} />
+
+				<Route path="/login" element={<Login/>}/>    
+
+        <Route path="/cadastro" element={<Cadastro/>}/>
+        <Route path='/cadastro2' element={<Cadastro2/>}/>
+        <Route path='/cadastro3' element={<Cadastro3/>}/>
+        <Route path='/cadastro/cuidador' element={<Cuidador1/>}/>
+        <Route path='/cadastro/cuidador2' element={<Cuidador2/>}/>
+        <Route path='/cadastro/cuidador3' element={<Cuidador3/>}/>
         <Route path='/cadastro/concluido' element={<Finalizado/>}/>
+
         <Route path='/chat' element={<Chat/>}/>
-        <Route path='/editar/perfil' element={<Editperfil/>}/>
-        <Route path='/cadastroAdm' element={<CadastroAdm/>}/>
-        <Route path='/cadastro/cuidador2' element={<Cuidador2 />} />
-        <Route path='/cadastro/cuidador3' element={<Cuidador3 />} />
-        <Route path='/procurar' element={<Procurar />} />
-        <Route path='/cadastro/concluido' element={<Finalizado/>}/>
-        <Route path='/chat' element={<Chat/>}/>
-        <Route path='/favorito' element={<Favorito/>}/>
+        <Route path='/procurar' element={<Procurar/>}/>
+        <Route path='/usuarios/perfil' element={<Perfil/>}/>
         <Route path='/atualizar/usuario' element={<AtualizarPerfil/>}/>
+        <Route path='/favoritos' element={<Favorito/>}/>
+
+        <Route path='/admin/cadastro' element={<CadastroAdm/>}/>
+        <Route path='/admin/editar' element={<Editperfil/>}/>
         <Route path='/admin/analiseDocs' element={<AnaliseDocs/>}/>
-        <Route path='/perfil' element={<Perfil/>}/>
-        <Route path='/admin/dash' element={<Perfil/>}/>
+        <Route path='/admin/dashboard' element={<Dashboard/>}/>
       </Routes>
     </BrowserRouter>
   );
