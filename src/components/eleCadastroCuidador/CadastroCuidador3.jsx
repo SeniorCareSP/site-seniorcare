18722402d8c55fae959863bd912afc44cf7cc707
igor-vieira-car <igor.carvalho@sptech.school>
2024-10-21 19:19:22 -0300

    Cadastrod e imagem

--- src/components/eleCadastroCuidador/CadastroCuidador3.jsx
+++ src/components/eleCadastroCuidador/CadastroCuidador3.jsx
@@ -6,7 +6,6 @@ import Title from '../tituloCadastro/Title';
 import ButtonAzul from '../botao/BotaoAzul';
 import ButtonBranco from '../botao/BotaoBranco';
 import Calendario from '../calendario/Calendario';
-import apiUsuario from "../../api/Usuario/apiUsuario";
 import apiCuidador from "../../api/Usuario/apiCuidador";
 import apiResponsavel from '../../api/Usuario/apiResponsavel';
 import axios from 'axios'; // Importar o axios
@@ -15,15 +14,11 @@ function CadastroCuidador3() {
     const navigate = useNavigate();
     const [calendario, setCalendario] = React.useState(Array(7).fill().map(() => Array(3).fill(false)));
     const [selectedFile, setSelectedFile] = useState(null);
-    const [filename, setFilename] = useState('');
-    const [response, setResponse] = useState('');
     const [idUsuario, setIdUsuario] = useState('');
     const dadosCadastro = localStorage.getItem("cadastro");
 
     const handleSave = async () => {
-
         try {
-
             if (dadosCadastro) {
                 const json = JSON.parse(dadosCadastro);
                 json.agendas = { "disponibilidade": calendario };
@@ -44,7 +39,6 @@ function CadastroCuidador3() {
                         setIdUsuario(idUsuario);
 
                         const formData = new FormData();
-                        
                         formData.append('file', selectedFile, `${idUsuario}.jpg`);
 
                         try {
@@ -78,7 +72,6 @@ function CadastroCuidador3() {
         }
     };
 
-
     const handleBack = () => {
         if (dadosCadastro.tipoDeUsuario === "CUIDADOR") {
             navigate("/cadastro/cuidador2");
@@ -87,9 +80,12 @@ function CadastroCuidador3() {
         }
     };
 
-
     const handleFileChange = (event) => {
         setSelectedFile(event.target.files[0]);
+    };
+
+    const handleRemoveImage = () => {
+        setSelectedFile(null); // Remove a imagem selecionada
     };
 
     return (
@@ -99,20 +95,32 @@ function CadastroCuidador3() {
                 <Stack spacing={3}>
                     <Title />
                     <Stack spacing={2} className={Style["itens"]}>
-
                         <h3>Envie uma foto:</h3>
 
-                        <Stack direction="row" justifyContent={"center"}>
-                            <ButtonAzul component="label" role={undefined} variant="contained" tabIndex={-1}>
-                                Imagem
-                                <input type="file" onChange={handleFileChange} style={{ display: 'none' }} />
-                            </ButtonAzul>
-                        </Stack>
+                        {!selectedFile && ( // O botão só aparece se não houver uma imagem selecionada
+                            <Stack direction="row" justifyContent={"center"}>
+                                <ButtonAzul component="label" role={undefined} variant="contained" tabIndex={-1}>
+                                    Imagem
+                                    <input type="file" onChange={handleFileChange} style={{ display: 'none' }} />
+                                </ButtonAzul>
+                            </Stack>
+                        )}
+
+                        {selectedFile && (
+                            <div style={{ textAlign: 'center' }}>
+                                <h4>Nome do arquivo: {selectedFile.name} - {(selectedFile.size / 1024).toFixed(2)} KB</h4>
+                                
+                                <ButtonBranco onClick={handleRemoveImage} style={{ marginTop: '10px' }}>
+                                    Remover Imagem
+                                </ButtonBranco>
+                            </div>
+                        )}
 
                         <Calendario onChange={setCalendario} />
-
-                        <ButtonAzul onClick={handleSave}>Concluir</ButtonAzul>
-                        <ButtonBranco onClick={handleBack}>Voltar</ButtonBranco>
+                        <Stack direction="row" spacing={4}>
+                            <ButtonAzul onClick={handleSave}>Concluir</ButtonAzul>
+                            <ButtonBranco onClick={handleBack}>Voltar</ButtonBranco>
+                        </Stack>
                     </Stack>
                 </Stack>
             </div>
