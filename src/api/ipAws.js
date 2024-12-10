800689279a5bc35712c8d73b96708ad60dc37762
igor-vieira-car <igor.carvalho@sptech.school>
2024-12-10 15:51:52 -0300

    Ajustando ip aws

--- src/api/ipAws.js
+++ src/api/ipAws.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// const ip =  "54.161.55.207"; // ip aws
-const ip = "localhost:8080"; // local
+const ip =  "18.213.14.124"; // ip aws
+// const ip = "localhost:8080"; // local
 
 export default ip;
